f0966fc7b92782f60009b4a601408cd34510a531
fatima-noorie <fatima_noor507@yahoo.com>
2016-05-22 23:11:54 +0500

    Add files via upload
    assignment 3 
    extra creditpart 2

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,120 @@
-//algos and programming
-//2-16
-//Arithmetic) Write a program that asks the user to enter two numbers,obtains the two numbers from the user and prints the sum
-//,product,difference, and quotient of the two numbers.
+//essay topic :our fake perception of social acceptance 
 #include<iostream>
+#include<string>
 using namespace std;
-int main()
+class human
 {
-	int a, b,sum, product, difference, quotient;     //initiaizing the vriables
-	cout << "enter first integer=" << endl;          //taking data from the user
-	cin >> a;
-	cout << "enter second integer" << endl;
-	cin >> b;                                        //calculating the req things
-	sum = a + b;
-	product = a*b;
-	difference = a - b;
-	quotient = a / b;
-	cout << "sum=" << sum << endl;
-	cout << "product=" << product << endl;            //displaying the results
-	cout << "difference=" << difference << endl;
-	cout << "quotient=" << quotient << endl;
-	system("pause");
-
-
+private://you can add other stuff like hair color ,and religious affiliations
+	string g;//gender
+	string nation;
+	string skin_color;
+	string edu;
+	float height;//in inches
+	float mass;//in lbs
+	string r;//religion
+	int status;//out of 100;//measure of social acceptance
+public:
+	human(string n = " ", string nat = " ", string s = "white", string a = " ", float b = 0, float e = 0, string c = "atheist", int d = 0) :g(n), nation(nat), skin_color(s), edu(a), height(b), mass(e), r(c), status(d)
+	{}
+	void set_info()
+	{
+		cout << "enter particulars" << endl;
+		cout << "enter your gender(m/f) " << endl;
+		cin >> g;
+		cout << "enter your skin color(black/brown/white) " << endl;
+		cin >> skin_color;
+		cout << "enter your education (doc/eng) " << endl;
+		cin >> edu;
+		cout << "enter your height " << endl;
+		cin >> height;
+		cout << "enter your mass " << endl;
+		cin >> mass;
+		cout << "enter your religion (athiest/monogamist/communist)" << endl;
+		cin >> r;
+	}
+	void get_info()
+	{
+		cout << "gender" << g << endl;
+		//basically afunction to display info complete it yourself 
+	}
+	float BMI()
+	{
+		return mass*0.45 / height / height / 0.025 / 0.025;
+	}
+	int social_acceptance()
+	{
+		if (nation == "asian")
+		{
+			if (g == "f")
+			{
+				if (BMI() <= 18.5)
+				{
+					status = status + 30;
+				}
+			}
+			else
+			{
+				if (BMI() <= 24)
+				{
+					status = status + 40;
+				}
+			}
+			if (skin_color == "white")
+			{
+				status = status + 40;
+			}
+			if (edu == "doc" || edu == "eng")
+			{
+				status = status + 10;
+			}
+			if (r == "monogamist" || r == "communist")
+			{
+				status = status + 10;
+			}
+			else if (r == "athiest")
+			{
+				status = status - 10;
+			}
+		}
+		if (nation == "europian")
+		{
+			if (g == "f")
+			{
+				if (BMI() <= 18.5)
+				{
+					status = status + 30;
+				}
+			}
+			else
+			{
+				if (BMI() <= 24)
+				{
+					status = status + 40;
+				}
+			}
+			if (skin_color == "white")
+			{
+				status = status + 40;
+			}
+			if (edu == "doc" || edu == "eng")
+			{
+				status = status + 10;
+			}
+			if (r == "monoathism" || r == "communist")
+			{
+				status = status + 10;
+			}
+			else if (r == "athiest")
+			{
+				status = status - 10;
+			}
+		}
+		return status;
+	}
+};
+void main()
+{
+	human fatima;
+	fatima.set_info();
+	cout << fatima.social_acceptance() << endl;
 }
